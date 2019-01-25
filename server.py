13b29d57152523c2d2b71ef1f8a43c8779d51889
Nicholas Whitefield <whitefie@ualberta.ca>
2019-01-25 09:10:20 -0700

    small changes

--- server.py
+++ server.py
@@ -5,6 +5,20 @@ from datetime import datetime
 from time import mktime
 import os
 
+# TODO: Refer to github for more details
+#   - Return a status code of “405 Method Not Allowed” for any method you
+#   cannot handle (POST/PUT/DELETE)
+#   - Provide a screenshot
+#   - I can check out the source code via an HTTP git URL
+#   - License your webserver properly (use an OSI approved license)
+#       - Put your name on it!
+
+# REVIEW:
+#   - The webserver works with Firefox and Chromium
+#   - The webserver can be run using the runner.sh file
+#   - Must run in the undergrad lab (Ubuntu 12.04)
+
+
 
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
@@ -31,29 +45,27 @@ import os
 # run: python freetests.py
 
 
-REDIRECTS = {
-"":""
-}
 ROOT = "www"
-HOST, PORT = "localhost", 8080
+HOST = "localhost"
+PORT = 8080
+SERVER = "Nicks_Macbook_Pro"
+DEBUG = True
 
-
-# try: curl -v -X GET http://127.0.0.1:8080/
 class Response():
 
     def __init__(self, status, body):
         self.status = status
         self.body = body
         self.header_dic = {
-        "Server":"Nicks_Macbook_Pro",
+        "Server":SERVER,
         "Date":self.get_date(),
         "Content-Type":None,
         "Content-Length":str(len(self.body.encode('utf-8'))),
         }
 
-    """
-    https://stackoverflow.com/questions/225086/rfc-1123-date-representation-in-python
-    """
+    # ----- CITATION:-----
+    # LINK: https://stackoverflow.com/questions/225086/rfc-1123-date-representation-in-python
+    # USERS: Florian Bösch
     def get_date(self):
         now = datetime.now()
         stamp = mktime(now.timetuple())
@@ -61,6 +73,9 @@ class Response():
 
 
     def header_to_string(self):
+        """
+        Converts header_dic into an HTTP/1.1 compliant string
+        """
         header_string = self.status + "\r\n"
 
         for key, value in self.header_dic.items():
@@ -78,30 +93,27 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print("--------- RECIEVED ---------")
-        print(self.data.decode('utf-8'))
-        print()
-
+        # print("--------- RECIEVED ---------")
+        # print(self.data.decode('utf-8'))
+        # print()
         request_dic = {}
         request_list = self.data.decode('utf-8').split("\r\n")
         request_line = request_list[0]
+
+        if DEBUG:
+            print("---- NEW REQUEST ----")
+            print(request_line)
 
         for i in range(1, len(request_list), 1):
             key, value = request_list[i].split(":", 1)
             request_dic[key] = value
 
-        print(request_line)
         method, uri, http_version = request_line.split()
-
         requested_path = ROOT + uri
-        print("requested_path = {}".format(requested_path))
-        """ Authenticate path permission """
         file = self.check_file_access(requested_path)
-        print("FILE DIC = {}".format(file))
 
         if file["EXISTS"] and file["AUTH"]:
-            with open(requested_path, 'r') as file_obj:
-                self.handle_200_response(file_obj)
+            self.handle_200_response(requested_path)
 
         elif file["EXISTS"] and not file["AUTH"]:
             """ could do 403 forbidden but tests expect 404 """
@@ -111,10 +123,8 @@ class MyWebServer(socketserver.BaseRequestHandler):
 
             redirect = self.reroute(uri)                                       # WARNING: test this well
             routed_file_perm = self.check_file_access(ROOT + redirect)
-            print("---------- REDIRECT = {} -----------".format(redirect))
 
             if routed_file_perm["EXISTS"] and routed_file_perm["AUTH"]:
-                print("301 ISSUED----------------------")
                 self.handle_301_response(redirect)
 
             elif routed_file_perm["EXISTS"] and not routed_file_perm["AUTH"]:
@@ -124,37 +134,45 @@ class MyWebServer(socketserver.BaseRequestHandler):
                 self.handle_404_response()
 
 
-    def handle_200_response(self, file):
-        status = "HTTP/1.1 200 OK"
-        body = file.read()
-        type = file.name.split(".")[-1]
+    def handle_200_response(self, file_path):
 
-        """ response object """
-        res_obj = Response(status, body)
-        res_obj.header_dic["Content-Type"] = "text/{}".format(type)
-        response_string = res_obj.response_string()
+        with open(file_path, 'r') as file_obj:
+            status = "HTTP/1.1 200 OK"
+            body = file_obj.read()
+            type = file_obj.name.split(".")[-1]
 
-        self.request.sendall(bytearray(response_string, 'utf-8'))
-        print()
-        print("DATA SENT")
+            # response object
+            res_obj = Response(status, body)
+            res_obj.header_dic["Content-Type"] = "text/{}".format(type)
+            response_string = res_obj.response_string()
+
+            self.request.sendall(bytearray(response_string, 'utf-8'))
+
+            if DEBUG:
+                print("-------- HEADER SENT --------")
+                print(res_obj.header_to_string())
 
 
     def handle_301_response(self, file_route):
         status = "HTTP/1.1 301 Moved Permanently"
 
         res_obj = Response(status, "ITEM WAS MOVED PERMANENTLY\n")
-        """ type should be 301 respones body type, not type of the file requested """
+        # 'Content-Type' should be 301 respones body type, not type of the file requested
         res_obj.header_dic["Content-Type"] = "text/plain"
         res_obj.header_dic["Location"] = "http://{}:{}{}".format(HOST, str(PORT), file_route)
         response_string = res_obj.response_string()
 
         self.request.sendall(bytearray(response_string, 'utf-8'))
-        print()
-        print("DATA SENT")
-        print(response_string)
+
+        if DEBUG:
+            print("-------- HEADER SENT --------")
+            print(res_obj.header_to_string())
 
 
     def handle_404_response(self):
+        """
+        Issues a 404 response
+        """
         status = "HTTP/1.1 404 Not Found"
 
         res_obj = Response(status, "ERROR 404 NOT FOUND\n")
@@ -162,12 +180,23 @@ class MyWebServer(socketserver.BaseRequestHandler):
         response_string = res_obj.response_string()
 
         self.request.sendall(bytearray(response_string, 'utf-8'))
-        print()
-        print("DATA SENT")
-        print(response_string)
+
+        if DEBUG:
+            print("-------- HEADER SENT --------")
+            print(res_obj.header_to_string())
 
 
     def check_file_access(self, requested_path):
+        """
+        Determines if a file exists and if the client has access to the file
+        Arguments:
+            requested_path : str
+                - path to a file
+        Return:
+            valid_file : dict
+                - 'EXISTS' : bool
+                - 'AUTH' : bool
+        """
         valid_file = {
         "EXISTS":os.path.isfile(requested_path),
         "AUTH":self.in_directory(requested_path, ROOT),
@@ -199,18 +228,16 @@ class MyWebServer(socketserver.BaseRequestHandler):
         """
         Edits the passed uri to more-likely represent a findable file
         """
-
         if uri[-1] == '/':
             """ directory suspected, default index.html """
             uri += "index.html"
-        elif len(uri.split('.')):
+        elif len(uri.split('.')) == 1:
             """ directory suspected, default index.html """
             uri += "/index.html"
 
         return uri
 
-
-
+# try: curl -v -X GET http://127.0.0.1:8080/
 if __name__ == "__main__":
 
     socketserver.TCPServer.allow_reuse_address = True
